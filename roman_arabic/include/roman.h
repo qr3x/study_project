f9e86fff5a79cd5770b717df255b358d4a2aad38
Vlad Kishkin <vladkishkin1@yandex.ru>
2021-02-19 01:26:39 +0300

    ready roman.h

--- roman_arabic/include/roman.h
+++ roman_arabic/include/roman.h
@@ -6,26 +6,80 @@
 using namespace std;
 
 
-struct roman {
+struct roman
+{
     string value;
-public:
+
+    // Конструкторы
     roman() : value("") {};
     roman(char val)
     {
         this->value = val;
     }
     roman(string val) : value(val) {};
+
+    // Операторы ввода-вывода
+    friend istream& operator>>(istream& in, roman& rom)
+    {
+        char romNumbers[7] = { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+
+        string str;
+        in >> str;
+
+        // Проверка строки, если там есть символы кроме romNumbers, то выдаем ошибку
+        for (int i = 0; i < str.size(); i++)
+            for (int j = 0; j < 7; j++)
+            {
+                if (str[i] == romNumbers[j])
+                    break;
+
+                //  Если прошло все итерации и не брейкнулось
+                // => этот символ не входит в romNumbers
+                if (j == 6)
+                    throw logic_error("Invalid roman numbers: the string includes other symbols");
+            }
+
+        rom.value = str;
+
+        return in;
+    }
+    friend ostream& operator<<(ostream& out, const roman& rom)
+    {
+        out << rom.value;
+
+        return out;
+    }
+
 };
 
-struct arabic {
+struct arabic
+{
     int value;
-public:
+
+    // Конструкторы
     arabic() : value(0) {};
     arabic(int val) : value(val) {};
+
+    // Операторы ввода-вывода
+    friend istream& operator>>(istream& in, arabic& rom)
+    {
+        int val;
+        in >> val;
+        rom.value = val;
+
+        return in;
+    }
+    friend ostream& operator<<(ostream& out, const arabic& rom)
+    {
+        out << rom.value;
+
+        return out;
+    }
 };
 
 
-class Convertor {
+class Convertor
+{
 public:
     // Арабские в римские
     roman toRoman(arabic& t);    // Для арабских цифр (структуры)
@@ -33,11 +87,6 @@ public:
     // Римские в арабские
     arabic toArabic(roman& t);   // Для римских цифр (структуры)
     arabic toArabic(string& t);  // Для обычной строки, в которой содержатся римские цифры
-
-    //bool check(roman &t);      // IIII - IV
-
-    // оператор ввода-вывода, либо print
-
 };
 
 #endif  // INCLUDE_ROMAN_H_
